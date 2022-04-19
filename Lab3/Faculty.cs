88317a346e33a6a9f208d08b9646d35699a62d27
Tima0102040 <voznesenskiytim@gmail.com>
2022-04-19 20:48:41 +0300

    Lab3

---
+++ Lab3/Faculty.cs
@@ -0,0 +1,75 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Lab3
+{
+    public class Faculty
+    {
+        private string nameFaculty;
+        private int numbCafed;
+        private int numbSpec;
+        private int numbStudSpec;
+        public string path="/Users/timofejvoznesenskij/RiderProjects/Lab3/Lab3/univer.txt";
+        
+        
+        public Faculty()
+        {
+            nameFaculty = "";
+            numbCafed = 0;
+            numbSpec = 0;
+            numbStudSpec = 0;
+        }
+
+        public Faculty(string nameFaculty, int numbCafed, int numbSpec, int numbStudSpec)
+        {
+            this.nameFaculty = nameFaculty;
+            this.numbCafed = numbCafed;
+            this.numbSpec = numbSpec;
+            this.numbStudSpec = numbStudSpec;
+        }
+
+        public string NameFaclty
+        {
+            set { nameFaculty = value; }
+            get { return nameFaculty; }
+        }
+
+        public int NumbCafed
+        {
+            set { numbCafed = value; }
+            get { return numbCafed; }
+        }
+
+        public int NumbSpec
+        {
+            set { numbSpec = value; }
+            get { return numbSpec; }
+        }
+
+        public int NumbStudSpec
+        {
+            set { numbStudSpec = value; }
+            get { return numbStudSpec; }
+        }
+        
+        public static void ReadingTxt(string path)
+        {
+            StreamReader reader = new StreamReader(path);
+        }
+
+        public static void WritingTxt(string path)
+        {
+            StreamWriter writer = new StreamWriter(path);
+
+            var faculty = new Dictionary<int, Faculty>()
+            {
+                {111, new Faculty {nameFaculty = "IPS", numbCafed = 5, numbSpec = 10, numbStudSpec = 100}},
+                {112, new Faculty {nameFaculty = Console.ReadLine(), numbCafed = 18, numbSpec = 20, numbStudSpec = 120}},
+            };
+
+            
+        }
+    }
+}
---
+++ Lab3/University.cs
@@ -0,0 +1,61 @@
+namespace Lab3
+{
+    public class University
+    {
+        private string name;
+        private string address;
+        private int numbFaclt;
+        private int studOnFaclt;
+        private int rating;
+        
+        public University()
+        {
+            name = "";
+            address = "";
+            numbFaclt = 0;
+            studOnFaclt = 0;
+            rating = 0;
+        }
+
+        public University(string name, string address, int numbFaclt, int studOnFaclt, int rating)
+        {
+            this.name = name;
+            this.address = address;
+            this.numbFaclt = numbFaclt;
+            this.studOnFaclt = studOnFaclt;
+            this.rating = rating;
+        }
+
+        public string Name
+        {
+            set { name = value; }
+            get { return name; }
+        }
+
+        public string Address
+        {
+            set { address = value; }
+            get { return address; }
+        }
+
+        public int NumbFaclt
+        {
+            set { numbFaclt = value; }
+            get { return numbFaclt; }
+        }
+
+        public int StudOnFaclt
+        {
+            set { studOnFaclt = value; }
+            get { return studOnFaclt; }
+        }
+
+        public int Rating
+        {
+            set { rating = value; }
+            get { return rating; }
+        }
+        
+        
+    }
+}
