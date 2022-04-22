f0a39408b16fdfcb8c64e899f28e6cc4575ccd18
Tima0102040 <voznesenskiytim@gmail.com>
2022-04-22 20:17:16 +0300

    New Method Financing

--- Lab3/University.cs
+++ Lab3/University.cs
@@ -11,14 +11,16 @@ namespace Lab3
         private int numbFaclt;
         private int studOnFaclt;
         private int rating;
+        
         public int scopus;
         public int top200;
         public int balzno;
+        
         public int firstPosition;
         public int secondPosition;
         public int thirdPosition;
-        
-        
+
+        public int financing;
 
         public University()
         {
@@ -71,27 +73,27 @@ namespace Lab3
         public void Scopus()
         {
             Random rnd = new Random();
-            firstPosition = rnd.Next(0, 200);
-            secondPosition = rnd.Next(0, 200);
-            thirdPosition = rnd.Next(0, 200);
+            firstPosition = rnd.Next(0, 40);
+            secondPosition = rnd.Next(0, 40);
+            thirdPosition = rnd.Next(0, 40);
             scopus = firstPosition + secondPosition + thirdPosition;
         }
 
         public void Top200()
         {
             Random rnd = new Random();
-            firstPosition = rnd.Next(0, 200);
-            secondPosition = rnd.Next(0, 200);
-            thirdPosition = rnd.Next(0, 200);
+            firstPosition = rnd.Next(0, 40);
+            secondPosition = rnd.Next(0, 40);
+            thirdPosition = rnd.Next(0, 40);
             top200 = firstPosition + secondPosition + thirdPosition;
         }
 
         public void BalZno()
         {
             Random rnd = new Random();
-            firstPosition = rnd.Next(0, 200);
-            secondPosition = rnd.Next(0, 200);
-            thirdPosition = rnd.Next(0, 200);
+            firstPosition = rnd.Next(0, 40);
+            secondPosition = rnd.Next(0, 40);
+            thirdPosition = rnd.Next(0, 40);
             balzno = firstPosition + secondPosition + thirdPosition;
         }
 
@@ -99,6 +101,20 @@ namespace Lab3
         {
             int ratingg = scopus + balzno + top200;
             return ratingg;
+        }
+
+        public int Financing( string city)
+        {
+            if (rating > 150 && (city == "Kyiv" || city == "Lviv" || city == "Kharkiv"))
+                financing = 5000 * studOnFaclt;
+            
+            else if (rating < 150 && rating > 100 && (city == "Kyiv" || city == "Lviv" || city == "Kharkiv"))
+                financing = 3800 * studOnFaclt;
+            
+            else if (rating < 150)
+                 financing = 3000 * studOnFaclt;
+            
+            return financing;
         }
         
     }
